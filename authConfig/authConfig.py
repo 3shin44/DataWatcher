a56625d14092bfc3ba301e09535b52323f863c49
Rico Huang <3shin44@gmail.com>
2024-04-30 00:11:49 +0800

    create basic setting

---
+++ authConfig/authConfig.py
@@ -0,0 +1,13 @@
+authConfig = {
+    # mySQL Database
+    "username": "root",
+    "password": "root",
+    "hostname": "localhost",
+    "port": "3306",
+    "database_name": "",
+    # LINE 
+    "LINE_TOKEN": "",
+    "user_id": ""
+    # finMind
+    "finMind_TOKEN": ""
+}
